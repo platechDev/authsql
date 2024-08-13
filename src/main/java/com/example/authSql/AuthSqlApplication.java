a4d489d8d66e68eea6dc23e979dc0b8d025595b1
axlMax <axlyeyo2000@gmail.com>
2024-08-12 21:09:44 -0500

    init

---
+++ src/main/java/com/example/authSql/AuthSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.authSql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authSql/AuthSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authSql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
